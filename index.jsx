5f80bf62449ddb54fa877e801462a1a3b76402c3
Ndifreke Etukudo <ndifreke.etukudo@decagon.dev>
2022-12-09 22:18:58 +0100

    initial commit

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from 'https://cdn.skypack.dev/react?min';
+import ReactDom from 'https://cdn.skypack.dev/react-dom?min';
+import moment from 'https://cdn.skypack.dev/moment?min';
+
+const DayNames ={
+    1: 'Mon',
+    2:'Tue'
+}
